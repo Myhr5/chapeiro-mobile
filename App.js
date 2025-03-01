dac6eaf3b39c9178a01476c9ea481d9324266b0b
Mirele Fernandes <mirele.mrf@gmail.com>
2025-03-01 16:55:32 -0300

    feat: adjust app.js file

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
-import { Text } from 'react-native';
-import { styles } from "./style.js";
-import Button from "./src/components/button/button.jsx";
+import Login from "./src/screens/login/login.jsx";
+import Register from "./src/screens/register/register.jsx";
 
 export default function App() {
 
   return (<>
-    <Text style={styles.text}>Hello World</Text>
-
-    <Button text="Acessar" />
+    <Register />
   </>
   );
 }
+
