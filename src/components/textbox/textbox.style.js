ebd1cd9a2bff2507533531352617c4a9e742385d
Mirele Fernandes <mirele.mrf@gmail.com>
2025-03-01 16:45:31 -0300

    feat: create textbox

---
+++ src/components/textbox/textbox.style.js
@@ -0,0 +1,18 @@
+import {colors, font_size} from "../../constants/theme"
+
+export const styles = {
+    input: {
+        width: "100%",
+        backgroundColor: colors.white,
+        padding: 10,
+        borderRadius: 6,
+        borderWidth: 1,
+        borderColor: colors.light_gray
+    },
+    label: {
+        marginLeft: 5,
+        color: colors.dark_gray,
+        fontSize: font_size.md,
+        marginBottom: 4
+    }
+}
