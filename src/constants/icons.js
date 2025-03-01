bd36eed24f954f7d22b59ca2e209f94464911670
Mirele Fernandes <mirele.mrf@gmail.com>
2025-03-01 17:46:34 -0300

    feat: add icons

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,6 +1,24 @@
+import abaFavorito from "../assets/icons/aba-favorito.png"
+import abaHome from "../assets/icons/aba-home.png"
+import abaPedidos from "../assets/icons/aba-pedidos.png"
+import abaPerfil from "../assets/icons/aba-perfil.png"
 import back from "../assets/icons/back.png"
+import cart from "../assets/icons/cart.png"
 import remove from "../assets/icons/delete.png"
-import logoLong from "../assets/icons/logo-long.png";
+import adress from "../assets/icons/endereco.png"
+import favoriteFullColorless from "../assets/icons/favorito-full.png"
+import favoriteFullColorfull from "../assets/icons/favorito-full2.png"
+import favorite from "../assets/icons/favorito.png"
+import location from "../assets/icons/location.png"
+import logoLong from "../assets/icons/logo-long.png"
+import logo from "../assets/icons/logo.png"
+import logout from "../assets/icons/logout.png"
+import plus from "../assets/icons/mais.png"
+import less from "../assets/icons/menos.png"
+import data from "../assets/icons/meus-dados.png"
+;
 
 
-export default {back, remove, logoLong};
+
+export default {abaFavorito, abaHome, abaPedidos, abaPerfil, back, cart, remove, adress, favoriteFullColorless,
+                   favoriteFullColorfull, favorite, location, logoLong, logo, logout, plus, less, data};
