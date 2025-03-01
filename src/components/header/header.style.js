851e72411e6b8d02ba346eaeaa4d985ae0cd4fa5
Mirele Fernandes <mirele.mrf@gmail.com>
2025-03-01 16:45:20 -0300

    feat: create header

---
+++ src/components/header/header.style.js
@@ -0,0 +1,17 @@
+import {colors, font_size} from "../../constants/theme"
+
+export const styles = {
+    header: {
+        width: "100%",
+        alignItems: "center"
+    },
+    logo: {
+        width: 170,
+        height: 50
+    },
+    title: {
+        color: colors.dark_gray,
+        fontSize: font_size.md,
+        textAlign: "center"
+    }
+}
