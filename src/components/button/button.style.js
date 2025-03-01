094eba07524bf301158defc5d6e6ee894d875b0a
Mirele Fernandes <mirele.mrf@gmail.com>
2025-03-01 16:26:09 -0300

    feat: create button style

---
+++ src/components/button/button.style.js
@@ -0,0 +1,14 @@
+import {colors, font_size} from "../../constants/theme"
+
+export const styles = {
+      btn: {
+            width: "100%",
+            backgroundColor: colors.yellow,
+      },
+      text: {
+            fontSize: font_size.md,
+            color: "white",
+            padding: 14,
+            textAlign: "center"
+      }
+}
