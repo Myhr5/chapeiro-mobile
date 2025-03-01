67c28f07fc8ddd56129a01b937e4029d3877a118
Mirele Fernandes <mirele.mrf@gmail.com>
2025-03-01 16:55:17 -0300

    fix: adjust register name

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,4 +1,6 @@
 import back from "../assets/icons/back.png"
 import remove from "../assets/icons/delete.png"
+import logoLong from "../assets/icons/logo-long.png";
 
-export default {back, remove};
+
+export default {back, remove, logoLong};
---
+++ src/screens/register/register.style.js
@@ -0,0 +1,30 @@
+import {colors, font_size} from "../../constants/theme"
+
+export const styles = {
+    container: {
+        flex: 1,
+        padding: 40,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    form: {
+        width: "100%",
+        marginBottom: 15
+    },
+    formGroup: {
+        width: "100%",
+        marginTop: 25,
+        marginBottom: 40
+    },
+    footer: {
+        width: "100%",
+        position: "absolute",
+        bottom: 0,
+        marginBottom: 25
+    },
+    footerText: {
+        textAlign: "center",
+        color: colors.dark_gray,
+        fontSize: font_size.md
+    }
+}
